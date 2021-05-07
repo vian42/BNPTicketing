dc4f913c7f5a20ca420799adcf424d5ab642a5b4
VianneyMaynadier <vianney.maynadier@arolla.fr>
2021-05-07 22:28:41 +0200

    add a default test

---
+++ src/test/java/org/kata/PricingTest.java
@@ -0,0 +1,14 @@
+package org.kata;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+class PricingTest {
+
+    @Test
+    void name() {
+        assertEquals(2,1+1);
+    }
+}
